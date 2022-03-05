5ba2255c2b5df6f52b55f46246c39dd0b6cfd012
Hatice-Kocabas <93576281+Hatice-Kocabas@users.noreply.github.com>
2022-03-05 17:22:57 +0300

    Add files via upload

---
+++ lib/screens/product_owerviev_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/cart.dart';
+import '../widgets/app_drawer.dart';
+import '../widgets/badge.dart';
+import '../widgets/products_grid.dart';
+import 'cart_screen.dart';
+
+enum FilterOptions {
+  favorites,
+  all,
+}
+
+class ProductsOverviewScreen extends StatefulWidget {
+  const ProductsOverviewScreen({Key? key}) : super(key: key);
+
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavorites = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Alışverişe başla!'),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.favorites) {
+                  _showOnlyFavorites = true;
+                } else {
+                  _showOnlyFavorites = false;
+                }
+              });
+            },
+            icon: const Icon(
+              Icons.more_vert,
+            ),
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                child: Text('Sadece favoriler'),
+                value: FilterOptions.favorites,
+              ),
+              const PopupMenuItem(
+                child: Text('Hepsini göster'),
+                value: FilterOptions.all,
+              ),
+            ],
+          ),
+          Consumer<Cart>(
+            builder: (_, cart, ch) => Badge(
+              child: ch!,
+              value: cart.itemCount.toString(),
+            ),
+            child: IconButton(
+              icon: const Icon(
+                Icons.shopping_cart,
+              ),
+              onPressed: () {
+                Navigator.of(context).pushNamed(CartScreen.routeName);
+              },
+            ),
+          ),
+        ],
+      ),
+      drawer: const AppDrawer(),
+      body: ProductsGrid(_showOnlyFavorites),
+    );
+  }
+}
